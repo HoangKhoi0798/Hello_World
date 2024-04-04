ef3e6c92b945bfdf021f670cc99a37a1801e007a
Khoi Nguyen <nkhoi0711@gmail.com>
2024-04-04 16:29:14 -0700

    Assignment 1

--- Program.cs
+++ Program.cs
@@ -1,11 +1,39 @@
-﻿namespace Hello_World
+﻿using System.Runtime.Intrinsics.X86;
+using System.Text.RegularExpressions;
+using System;
+
+namespace Hello_World
 {
     internal class Program
     {
+        //Hoang Khoi Gia Nguyen
+        //Computer Programming 120
+        //04/04/2024
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Hoang Khoi Gia Nguyen");
+            Console.WriteLine("Progamming 120");
+            Console.WriteLine("I listen to music and play video games to relax.");
+            Console.WriteLine();
             Console.WriteLine("Hello, World!");
             Console.WriteLine();
+
+            // What kind of error does a red squiggly line in your code represent?
+            // Ans: Compile Time Error.
+
+            // What is the difference between Console.Write() and Console.WriteLine()?
+            // Ans: Console.WriteLine() go to a new line while Console.Write() does not.
+
+            // What symbol is used to tell our code something is a string?
+            // Ans: Double Quotation
+
+            // What service do we use to save our code online?
+            // Ans: Github
+
+            // When you see { and }, this indicates a bunch of code is grouped together. This is called a?
+            // Ans: Code Block
+
         }
     }
 }
